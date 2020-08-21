6fd16b0c4eaeac0ab620e8b5a906a0a83b0eef08
rtiw <rtiw@host.docker.internal>
2020-08-21 17:37:52 +0530

    initial changes

---
+++ servertls/src/main/java/com/softag/servertls/ServerTlsApplication.java
@@ -0,0 +1,13 @@
+package com.softag.servertls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTlsApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ServerTlsApplication.class, args);
+	}
+
+}
